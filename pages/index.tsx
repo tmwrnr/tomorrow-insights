9cd32945bca89f4d4f0863ddafc697274ab46aab
Tim Werner <tm.wrnr+coding@pm.me>
2022-09-07 11:18:37 +0200

    Change footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -131,6 +131,7 @@ const Home: NextPage = () => {
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
             viewBox="0 0 1440 320"
+            className="lg:-mb-10"
           >
             <path
               fill="#8B5CF6"
@@ -138,14 +139,9 @@ const Home: NextPage = () => {
               d="M0,64L48,101.3C96,139,192,213,288,213.3C384,213,480,139,576,133.3C672,128,768,192,864,208C960,224,1056,192,1152,181.3C1248,171,1344,181,1392,186.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
             ></path>
           </svg>
-          <div className="bg-violet-500 p-4 text-violet-50 lg:-mt-20">
-            <div className="flex justify-center">
-              <a
-                href="https://github.com/tmwr"
-                target="_blank"
-                rel="noreferrer"
-                className="flex flex-col items-center rounded-lg p-2 hover:bg-violet-400 hover:text-white"
-              >
+          <div className="bg-violet-500 px-10 py-6 text-violet-50 md:pt-0">
+            <div className="mx-auto max-w-md">
+              <div className=" text-center text-xs">
                 <Image
                   layout="fixed"
                   src="https://avatars.githubusercontent.com/u/39673874?s=40&v=4"
@@ -154,18 +150,55 @@ const Home: NextPage = () => {
                   height="40px"
                   className="h-10 w-10 rounded-full"
                 />
-                <span className="mt-1 text-sm">Tim Werner</span>
-              </a>
-            </div>
-            <div className="text-center">
-              <a
-                href="https://github.com/tmwrnr/tomorrow-insights"
-                target="_blank"
-                rel="noreferrer"
-                className="text-xs hover:text-white hover:underline"
-              >
-                Open Source
-              </a>
+                <div className="mt-2">
+                  Made by{" "}
+                  <a
+                    href="https://github.com/tmwrnr"
+                    target="_blank"
+                    rel="noreferrer"
+                    className=" underline hover:text-white"
+                  >
+                    tmwrnr
+                  </a>
+                </div>
+              </div>
+              <div className="mt-6">
+                <h3 className="mb-1 text-center text-lg font-semibold text-violet-200">
+                  Links
+                </h3>
+                <ul className="space flex flex-wrap justify-center space-x-2 text-sm">
+                  <li>
+                    <a
+                      href="/example.csv"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:text-white hover:underline"
+                    >
+                      Beispiel CSV
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://github.com/tmwrnr/tomorrow-insights"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:text-white hover:underline"
+                    >
+                      Source Code
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.tomorrow.one/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:text-white hover:underline"
+                    >
+                      Tomorrow
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </footer>
